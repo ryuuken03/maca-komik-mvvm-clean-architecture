191a1ea0be4125f86ec364ace1a744aff8cf293c
= <=>
2024-02-02 14:14:15 +0700

    add BackupDataDB.kt

---
+++ app/src/main/java/mapan/developer/macakomik/domain/usecase/setting/BackupDataDB.kt
@@ -0,0 +1,18 @@
+package mapan.developer.macakomik.domain.usecase.setting
+
+import kotlinx.coroutines.flow.Flow
+import mapan.developer.macakomik.data.datasource.local.db.entity.ComicSaveEntity
+import mapan.developer.macakomik.domain.repository.DatabaseRepository
+import mapan.developer.macakomik.domain.usecase.BaseUseCase
+import javax.inject.Inject
+
+/***
+ * Created By Mohammad Toriq on 09/01/2024
+ */
+class BackupDataDB @Inject constructor(
+    private val databaseRepository: DatabaseRepository
+) : BaseUseCase<Unit, Flow<Int>>() {
+    override suspend fun execute(params: Unit) : Flow<Int> {
+        return databaseRepository.backupDataDB()
+    }
+}
