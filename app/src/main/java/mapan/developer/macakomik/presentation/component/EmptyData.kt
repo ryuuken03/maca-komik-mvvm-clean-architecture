450c24111a584305682b888706278fc230114480
= <=>
2024-02-12 16:21:39 +0700

    Update EmptyData.kt : add var outContent, update modifier and add Column

--- app/src/main/java/mapan/developer/macakomik/presentation/component/EmptyData.kt
+++ app/src/main/java/mapan/developer/macakomik/presentation/component/EmptyData.kt
@@ -1,10 +1,16 @@
 package mapan.developer.macakomik.presentation.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,19 +23,28 @@ import androidx.compose.ui.unit.dp
  * Created By Mohammad Toriq on 03/01/2024
  */
 @Composable
-fun EmptyData(message :String) {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(500.dp)
+fun EmptyData(message :String,outContent:Boolean = true) {
+    var modifier = Modifier.fillMaxSize()
+    if(outContent){
+        modifier.verticalScroll(rememberScrollState())
+    }
+    Column(
+        modifier = modifier,
     ) {
-        Text(
-            text = message,
-            color = Color.White,
+        Box(
+            contentAlignment = Alignment.Center,
             modifier = Modifier
                 .fillMaxWidth()
-                .wrapContentWidth(Alignment.CenterHorizontally)
-        )
+                .height(500.dp)
+//            .fillMaxHeight()
+        ) {
+            Text(
+                text = message,
+                color = Color.White,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentWidth(Alignment.CenterHorizontally)
+            )
+        }
     }
 }
